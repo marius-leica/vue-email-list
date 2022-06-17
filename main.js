44e21f2d4afb3a3b24c1287435a11ba9df9e5626
marius-leica <mariusleica@gmail.com>
2022-06-17 15:27:41 +0200

    done with an extra autodelete after 5 secconds

--- main.js
+++ main.js
@@ -9,6 +9,31 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 const app = new Vue({
   el: "#app",
-  data: {},
-  methods: {},
+  data: {
+    listaMails: [],
+  },
+  methods: {
+    checkMailsListLength() {
+      if (this.listaMails.length === 10) {
+        alert(
+          "Read them carrefuly becouse in 5 secconds everithing will be deleted"
+        );
+      }
+    },
+    getRandomMail() {
+      this.listaMails = [];
+      const url = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+      for (let i = 0; i < 10; i++) {
+        axios.get(url).then((axiosResp) => {
+          this.listaMails.push(axiosResp.data.response);
+          this.checkMailsListLength();
+          console.log(axiosResp.data.response);
+        });
+      }
+      setTimeout(() => {
+        this.listaMails = [];
+      }, 5000);
+    },
+  },
 });
